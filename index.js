2c0bb4315efd635654815e0bc05d0d3cfe92144e
rawr51919 <colton51919@gmail.com>
2023-11-18 12:20:00 -0400

    Add branch for if we need just ONE ID for arrays

--- index.js
+++ index.js
@@ -6,26 +6,12 @@ export default (arr, label) => {
 	let obj = {};
 	let result = arr.map(function(arr, index) {
 		digits.push(index);
-		if (typeof arr === 'object') {
-			let i = 0;
-			Object.keys(arr).forEach(function(key) {
-				let x = '';
-				digits.push(i);
-				x = '_' + key + 'Id';
-				arr[x] = hashids.encode(digits);
-				digits = digits.slice(0, 6);
-				console.log(digits);
-				i++;
-			});
-			return arr;
-		} else {
-			obj = {
-				value: arr,
-				id: hashids.encode(digits)
-			};
-			digits = digits.slice(0, 5);
-			return obj;
-		}
+		obj = {
+			value: arr,
+			id: hashids.encode(digits)
+		};
+		digits = digits.slice(0, 5);
+		return obj;
 	});
 	return result;
 };
